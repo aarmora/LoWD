909a09709e3e8191e5401892d3be715a02dcdfc5
Matt Lamm <armorforsleep15@hotmail.com>
2014-10-16 09:46:25 -0600

    descrition

--- LoWD/Controllers/HomeController.cs
+++ LoWD/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace LoWD.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application descrition page.";
 
             return View();
         }
