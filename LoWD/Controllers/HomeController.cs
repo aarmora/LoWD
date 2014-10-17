b22bf0b0da276346bbce39262808691a7760771f
Jordan Hansen <jbhansen84@gmail.com>
2014-10-17 16:48:35 -0600

    starting the addition

--- LoWD/Controllers/HomeController.cs
+++ LoWD/Controllers/HomeController.cs
@@ -4,15 +4,54 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LoWD.Models;
+using Newtonsoft.Json;
 
 namespace LoWD.Controllers
 {
     public class HomeController : Controller
     {
         private LoWDEntities db = new LoWDEntities();
+
+        public class user
+        {
+            public int? game_id { get; set; }
+            public int? user_id { get; set; }
+            public int? lord_id { get; set; }
+            public int? lord_pts { get; set; }
+            public int? corruption_pts { get; set; }
+            public int? gold_pts { get; set; }
+            public int? adv_pts { get; set; }
+            public int? quest_pts { get; set; }
+            public int? quest_qty { get; set; }
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult newGame(string[] users)
+        {
+            //turn the array of json objects into the user model.
+
+            //This works for a single user.  (it would be sent from the js without the [] and we'd catch it over here as just a string)
+            //var json = JsonConvert.DeserializeObject<user>(users);
+            
+
+
+            int count = 0;
+            foreach (string item in users)
+            {
+                count = count + 1;
+
+                Response.Write(count);
+
+                Response.Write(item);
+                
+            }
+
+
+            return new EmptyResult();
         }
 
         public ActionResult About()
--- LoWD/Scripts/AppScripts/homeController.js
+++ LoWD/Scripts/AppScripts/homeController.js
@@ -2,7 +2,27 @@
 (function(){
     
     var homeController = function($scope, $http){
-    
+        $scope.users = [];
+
+        $scope.addUser = function () {
+            if ($scope.users.length < 6) {
+                $scope.users.push(
+                    { "user_id": "", "lord_id": "", "lord_pts": "", "corruption_pts": "", "gold_pts": "", "adv_pts": "", "quest_pts": "", "quest_qty": "" }
+                );
+            }
+        }
+
+        $scope.removeUser = function (index) {
+            $scope.users.splice(index, 1)
+        }
+
+        $scope.submitGame = function () {
+            $http({
+                url: 'newGame',
+                method: 'POST',
+                params: {users:[$scope.users]}
+            })
+        }
 
     }
 
