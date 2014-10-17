a9f74f226eee91e9f406056a8dc28ca3ac4faf58
Matt Lamm <armorforsleep15@hotmail.com>
2014-10-17 12:40:10 -0600

    db connection

--- LoWD/Controllers/HomeController.cs
+++ LoWD/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LoWD.Models;
 
 namespace LoWD.Controllers
 {
     public class HomeController : Controller
     {
+        private LoWDEntities db = new LoWDEntities();
         public ActionResult Index()
         {
             return View();
