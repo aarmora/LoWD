21f524f555f265a9dfd39710104752536dcb0869
Matt Lamm <armorforsleep15@hotmail.com>
2014-12-06 09:41:43 -0700

    removed some junk

--- LoWD/Scripts/AppScripts/controllers.js
+++ LoWD/Scripts/AppScripts/controllers.js
@@ -97,15 +97,6 @@
 
     var lordStatsController = function ($scope, $http) {
 
-        $scope.items = [
-            { Id: "01", Name: "A", Price: "1.00", Quantity: "1" },
-            { Id: "02", Name: "B", Price: "10.00", Quantity: "1" },
-            { Id: "04", Name: "C", Price: "9.50", Quantity: "10" },
-            { Id: "03", Name: "a", Price: "9.00", Quantity: "2" },
-            { Id: "06", Name: "b", Price: "100.00", Quantity: "2" },
-            { Id: "05", Name: "c", Price: "1.20", Quantity: "2" }
-        ];
-
         $http({
             url: '/lowd/Home/getLordLeaderboard',
             method: 'GET'
